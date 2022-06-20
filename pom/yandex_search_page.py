53a9090100b0d9ab6548dc23a2aca7b01de8e153
Quastrado <Ataxyswriter@yandex.ru>
2022-06-20 23:13:28 +0300

    first testcase rewrite

--- pom/yandex_search_page.py
+++ pom/yandex_search_page.py
@@ -1,14 +1,23 @@
 from base.selenium_base import SeleniumBase
+from selenium.webdriver.common.keys import Keys
 
 
 class YandexSearchPage(SeleniumBase):
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
-        self.__input_search_xpath = '//*[@id="text"]' 
+        self.__central_input_search_xpath = '//*[@id="text"]'
+        self.__upper_input_search_xpath = '//*[@id="uniq16547008090681"]'
 
-    def see_input_search(self):
-        return self.is_visiable('xpath', self.__input_search_xpath, 'Search Input')
+    
+    def send_query_search(self, query):
+        central_input_search = self.is_present('xpath', self.__central_input_search_xpath)
+        central_input_search.send_keys(query)
+        central_input_search.send_keys(Keys.ENTER)
+        upper_input_search = self.is_visiable('xpath', self.__upper_input_search_xpath)
+        atr = upper_input_search.get_attribute('value')
+        return atr
+        
 
     def switch_disighn():
         pass
--- tests/test_yandex_search.py
+++ tests/test_yandex_search.py
@@ -1,14 +1,17 @@
 import pytest
 from pom.yandex_search_page import YandexSearchPage
-
+from .config import send_query_search_data
+import allure
 
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
 
-    def test_input_search_visibility(self):
-        yandex_search_page = YandexSearchPage(self.driver)
-        a = yandex_search_page.see_input_search() # for debug
-        assert a != None
-
-
+    @allure.feature('First test')
+    @allure.story('Go to results page')
+    @pytest.mark.parametrize('query', send_query_search_data)
+    def test_send_query_search(self, query):
+        with allure.step('Search page opening'):
+            yandex_search_page = YandexSearchPage(self.driver)
+        with allure.step('The page with the results for the query was opened'):
+            assert yandex_search_page.send_query_search(query) == query
 
