cac202faef310b0c3b7c666346517c612e051396
Quastrado <Ataxyswriter@yandex.ru>
2022-06-21 22:41:38 +0300

    checking the disappearance of elements from the page when changing the design

--- pom/yandex_search_page.py
+++ pom/yandex_search_page.py
@@ -1,13 +1,16 @@
 from base.selenium_base import SeleniumBase
 from selenium.webdriver.common.keys import Keys
+from .config import *
 
 
 class YandexSearchPage(SeleniumBase):
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
-        self.__central_input_search_xpath = '//*[@id="text"]'
-        self.__upper_input_search_xpath = '//*[@id="uniq16547008090681"]'
+        self.__central_input_search_xpath = central_input_search_xpath
+        self.__upper_input_search_xpath = upper_input_search_xpath
+        self.__switcher_class_name = 'footer3__disable-new-design-toggle'
+        self.left_logo_xpath = '/html/body/main/div[1]/a'
 
     
     def send_query_search(self, query):
@@ -15,10 +18,14 @@ class YandexSearchPage(SeleniumBase):
         central_input_search.send_keys(query)
         central_input_search.send_keys(Keys.ENTER)
         upper_input_search = self.is_visiable('xpath', self.__upper_input_search_xpath)
-        atr = upper_input_search.get_attribute('value')
-        return atr
+        value = upper_input_search.get_attribute(upper_input_search_value)
+        return value
         
 
-    def switch_disighn():
-        pass
+    def switch_disighn(self, xpath):
+        switcher = self.is_visiable('class_name', self.__switcher_class_name)
+        switcher.click()
+        element = self.is_visiable('xpath', xpath)
+
+
 
--- tests/test_yandex_search.py
+++ tests/test_yandex_search.py
@@ -1,12 +1,12 @@
 import pytest
 from pom.yandex_search_page import YandexSearchPage
-from .config import send_query_search_data
+from .config import *
 import allure
 
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
 
-    @allure.feature('First test')
+    @allure.feature('Test 1')
     @allure.story('Go to results page')
     @pytest.mark.parametrize('query', send_query_search_data)
     def test_send_query_search(self, query):
@@ -15,3 +15,12 @@ class TestHomepage:
         with allure.step('The page with the results for the query was opened'):
             assert yandex_search_page.send_query_search(query) == query
 
+    @allure.feature('Test 2')
+    @allure.story('Change of design to the old one')
+    @pytest.mark.parametrize('expected_exception, element_name, xpath', switch_desighn_data)
+    def test_switch_design(self, expected_exception, element_name, xpath):
+        with allure.step('Search page opening'):
+            with pytest.raises(expected_exception):
+                yandex_search_page = YandexSearchPage(self.driver)
+                with allure.step(f'Checking for the absence of an element: {element_name}'):
+                    yandex_search_page.switch_disighn(xpath) == True
