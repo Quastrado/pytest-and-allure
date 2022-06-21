5af2feda544e49b23ea3c83d5bc5535c386fcc85
Quastrado <Ataxyswriter@yandex.ru>
2022-06-21 23:11:40 +0300

    remove wrong function

--- abstract/not_found_exception_raiser.py
+++
@@ -1,9 +0,0 @@
-import http.client
-
-
-def check_url(url):
-    conn = http.client.HTTPConnection(url.netloc)
-    conn.request("HEAD", url.path)
-    r = conn.getresponse()
-    if r.status == 404:
-        raise Exception('Connection aborted')
