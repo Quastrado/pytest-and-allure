678af502c2058a9ebf305d8454259e30c55c231f
Quastrado <Ataxyswriter@yandex.ru>
2022-06-25 14:44:00 +0300

    positive and negative representation of link checking

--- tests/test_yandex_search.py
+++ tests/test_yandex_search.py
@@ -15,15 +15,31 @@ class TestHomepage:
         with allure.step('The page with the results for the query was opened'):
             assert yandex_search_page.send_query_search(query) == query
 
+
     @allure.feature('Checking for elements on the page when changing the design')
-    @allure.story('Change of design to the old one')
+    @allure.story('Change of design to the old one and try to find element')
     @pytest.mark.parametrize('expected_exception, element_name, xpath', check_for_links_data)
-    def test_switch_design(self, expected_exception, element_name, xpath):
+    def test_find_element_after_switch(self, expected_exception, element_name, xpath):
+        yandex_search_page = YandexSearchPage(self.driver)
+        try:
+            with allure.step(f'Checking for the absence of an element: {element_name}'):
+                yandex_search_page.find_element_after_switch(xpath) == True
+                not_found = False
+        except:
+            not_found = True
+        assert not_found
+
+    
+    @allure.feature('NEGATIVE Checking for elements on the page when changing the design')
+    @allure.story('Change of design to the old one and try to find element')
+    @pytest.mark.parametrize('expected_exception, element_name, xpath', check_for_links_data)
+    def test_find_element_after_switch_negative(self, expected_exception, element_name, xpath):
         with pytest.raises(expected_exception):
             yandex_search_page = YandexSearchPage(self.driver)
             with allure.step(f'Checking for the absence of an element: {element_name}'):
                 yandex_search_page.find_element_after_switch(xpath) == True
 
+    
     @allure.feature('Verification of the transition on the link on the page')
     @allure.story('Following links on the page')
     @pytest.mark.parametrize('link, link_xpath, expected_element_xpath', following_link_data)
